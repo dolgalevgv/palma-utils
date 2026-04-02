a44cea1586aff34264d72a30350f9bbf77aa01dd
Georgii Dolgalev <dolgalevgv@gmail.com>
2026-04-02 13:59:46 +0200

    Optionally specify local port

--- jupyter-lab-pixi.sh
+++ jupyter-lab-pixi.sh
@@ -3,12 +3,13 @@
 CYAN="\033[0;36m"
 COLOR_OFF="\033[0m"
 
+PORT="${1:-8888}"
 LABPORT=$(shuf -i 8000-8500 -n 1)
 TUNNELPORT=$(shuf -i 8501-9000 -n 1)
 TOKEN=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 49 | head -n 1)
 
-echo -e "${CYAN}ssh -L 8888:localhost:${TUNNELPORT} palma \"ssh -L ${TUNNELPORT}:localhost:${LABPORT} ${SLURMD_NODENAME} -N -4\"${COLOR_OFF}\n"
+echo -e "${CYAN}ssh -L ${PORT}:localhost:${TUNNELPORT} palma \"ssh -L ${TUNNELPORT}:localhost:${LABPORT} ${SLURMD_NODENAME} -N -4\"${COLOR_OFF}\n"
 
-echo -e "${CYAN}http://localhost:8888/lab?token=${TOKEN}${COLOR_OFF}\n"
+echo -e "${CYAN}http://localhost:${PORT}/lab?token=${TOKEN}${COLOR_OFF}\n"
 
 jupyter-lab --no-browser --port=${LABPORT} --ServerApp.token=${TOKEN}
